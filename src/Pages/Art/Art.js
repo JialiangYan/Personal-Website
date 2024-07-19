0b514a2dd97d09062efef2d6300755bda28b3cc0
yajialiang <240881275@qq.com>
2024-07-19 03:14:10 +0100

    update

--- src/Pages/Art/Art.js
+++ src/Pages/Art/Art.js
@@ -3,6 +3,7 @@ import { Row, Col, Container, Image, Stack, Badge } from 'react-bootstrap'
 import './Art.css'
 import a1 from '../../Assets/Art/a1.png'
 import a4 from '../../Assets/Art/a4.png'
+import mic from '../../Assets/mic.jpeg'
 
 function Art() {
   return (
@@ -10,7 +11,8 @@ function Art() {
       <div className="r-title">Misc 🦄</div>
       <div className="r-description">
         This page is still under construction. In my spare time, I love drawing
-        and listening to dreampop. My favorite artist is{' '}
+        while listening to music. I spend over 12,000 minutes each month
+        immersed in my favorite tunes 😬. My fav artist is{' '}
         <a
           href="https://open.spotify.com/artist/4wXchxfTTggLtzkoUhO86Q"
           className="highlight"
@@ -22,6 +24,9 @@ function Art() {
         .
       </div>
       <div className="a-container">
+        <div className="a-img">
+          <img src={mic} alt="" />
+        </div>
         <div className="a-img">
           <img src={a1} alt="" />
         </div>
